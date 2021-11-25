bf19d0b5ff18017917d41e5cd7ecd53ad9284ff8
shijunlin <miaozilsj@gmail.com>
2021-11-25 20:40:49 +0100

    update

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///test.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URI')
 db = SQLAlchemy(app)
 
 
